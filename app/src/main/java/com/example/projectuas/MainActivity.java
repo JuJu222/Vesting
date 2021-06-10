e5f6533e44fc05df130ff9e127cddacfc407e2b5
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2021-06-10 12:49:19 +0700

    Cleaned up code from unused lines

--- app/src/main/java/com/example/projectuas/MainActivity.java
+++ app/src/main/java/com/example/projectuas/MainActivity.java
@@ -33,23 +33,14 @@ import model.UserArray;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Intent intent;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_portfolio, R.id.navigation_news, R.id.navigation_profile)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
-
-        intent = getIntent();
 
         Companies.getListedCompanies().clear();
         Companies.getListedCompanies().add(new ListedCompany("MMM"));
